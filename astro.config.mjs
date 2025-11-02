17f9f61659aedb12c8d9347e107b8e55bc2dca92
Máximo García <63814427+d4str3k@users.noreply.github.com>
2025-11-02 20:31:56 +0100

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dennisklappe.github.io',
+  site: 'https://d4str3k.github.io',
   // Only use base path in production (GitHub Pages)
   base: process.env.NODE_ENV === 'production' ? '/astro-theme-terminal' : '/',
   integrations: [sitemap()],
